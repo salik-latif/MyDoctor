21e3cc4baf898e0ce83ac5453ef7e902d4f39a43
saliklatif <salik0331@gmail.com>
2017-07-11 12:21:44 -0700

    test

--- MyDoctorServices/MyDoctorServices/App_Start/WebApiConfig.cs
+++ MyDoctorServices/MyDoctorServices/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@ namespace MyDoctorServices
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
